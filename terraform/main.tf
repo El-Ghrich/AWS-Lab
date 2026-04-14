b203d180e30b4939f6778eb98f0b54806b4a3ebc
El-Ghrich Salman <elghrichsalman2004@gmail.com>
2026-04-14 13:45:00 +0100

    Suppression du cle de AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,7 +95,6 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "app_server" {
   ami           = "ami-098e39bafa7e7303d" # Ubuntu 22.04 LTS
   instance_type = "t2.micro"
-  
   # On attache tout ce qu'on a créé au-dessus :
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
