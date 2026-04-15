d79599f80359813dad1e161cc872dd1b36724cb6
El-Ghrich Salman <elghrichsalman2004@gmail.com>
2026-04-15 11:06:32 +0100

    Changed ami id

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,7 +93,7 @@ resource "aws_security_group" "web_sg" {
 # ------------------------------------------------------
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-098e39bafa7e7303d" # Ubuntu 22.04 LTS
+  ami           = "ami-03ed25db53d8de46c" 
   instance_type = "t2.micro"
   # On attache tout ce qu'on a créé au-dessus :
   subnet_id              = aws_subnet.public_subnet.id
