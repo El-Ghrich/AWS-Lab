54801da43210124e7dd0b5bac60243376d8d69e8
El-Ghrich Salman <elghrichsalman2004@gmail.com>
2026-04-15 20:28:09 +0100

    changed instance type to t3

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-03ed25db53d8de46c" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   # On attache tout ce qu'on a créé au-dessus :
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
